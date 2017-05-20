78aafb2b3ec6e99bbc41f33bc808108a956429e9
Evan Dorsky <evan.dorsky@icloud.com>
2017-05-20 14:09:08 -0700

    Write to multiple files!

--- arduino/sdmain/sdmain.ino
+++ arduino/sdmain/sdmain.ino
@@ -1,27 +1,49 @@
 #include <SPI.h>
 #include <SD.h>
 
-File myFile;
-
-void cardWrite(char *filename, char *contents) {
-    if (!SD.begin(3)) {
-        return;
+void cardWrite(char *filename, char *contents, bool debug) {
+    if (debug) {
+        Serial.print("Writing to file: ");
+        Serial.println(filename);
     }
 
-    myFile = SD.open(filename, FILE_WRITE);
+    File file;
+    file = SD.open(filename, FILE_WRITE);
 
-    if (myFile) {
-        myFile.println(contents);
-        myFile.close();
+    if (file) {
+        file.println(contents);
+        file.close();
+
+        if (debug) {
+            Serial.print("File written successfully: ");
+            Serial.println(filename);
+        }
     } else { //error
+        if (debug) {
+            Serial.print("Error writing to file: ");
+            Serial.println(filename);
+        }
         return;
     }
 }
 
 void setup() {
-    cardWrite("heyyy.txt", "what goes here boi");
+    Serial.begin(9600);
+    pinMode(13, OUTPUT);
+    SD.begin(3);
 }
 
+int i = 0;
+char filename[100];
 void loop() {
+    if (i < 10) {
+        sprintf(filename, "file_%i.txt", i);
+        cardWrite(filename, "hiiiiiii", true);
+        delay(15);
+        i++;
+    }
+    else {
+        digitalWrite(13, HIGH);
+    }
     // nothing happens after setup
 }
